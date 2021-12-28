96888e70dd765070ffafd90afa4fda41faf570c4
Yimy Zepeda <zepedayimy45@gmail.com>
2021-12-28 21:33:49 +0100

    tercero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarComponent } from './Tarea/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'listar',component:ListarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
